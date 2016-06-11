81b4e3902d4f28965e88dea5675fd7717b0a75b2
koterwong <331244921@qq.com>
2016-06-11 19:52:20 +0800

    忘了添加一个类。。。

---
+++ weather/src/main/java/com/koterwong/weather/receiver/LocalRegisterHelper.java
@@ -0,0 +1,52 @@
+package com.koterwong.weather.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
+
+/**
+ * ================================================
+ * Created By：Koterwong; Time: 2016/06/11 18:49
+ * <p/>
+ * Description:
+ * =================================================
+ */
+public class LocalRegisterHelper {
+
+    private Context mContext;
+    private static LocalRegisterHelper mInstance;
+
+    public static final String action = "com.koterwong.weather.Notification";
+
+    private LocalRegisterHelper(Context context) {
+        this.mContext = context;
+    }
+
+    public static LocalRegisterHelper getInstance(Context context) {
+        if (mInstance == null) {
+            synchronized (LocalRegisterHelper.class) {
+                if (mInstance == null) {
+                    return mInstance = new LocalRegisterHelper(context);
+                }
+            }
+        }
+        return mInstance;
+    }
+
+    public void send(BroadcastReceiver receiver, String action) {
+        this.register(receiver, action).sendBroadcast(new Intent(LocalRegisterHelper.action));
+    }
+
+    public void unRegister(BroadcastReceiver receiver){
+        LocalBroadcastManager instance = LocalBroadcastManager.getInstance(mContext);
+        instance.unregisterReceiver(receiver);
+    }
+
+    private LocalBroadcastManager register(BroadcastReceiver receiver, String action) {
+        LocalBroadcastManager instance = LocalBroadcastManager.getInstance(mContext);
+        instance.registerReceiver(receiver, new IntentFilter(action));
+        return instance;
+    }
+}
